ad659f81499b4dc1d704c8804fa904daa01f08ab
xg4 <xingor4@gmail.com>
2020-02-13 00:16:08 +0800

    feat: add music 163

--- src/config.ts
+++ src/config.ts
@@ -4,5 +4,7 @@ dotenv.config()
 
 export default {
   ACFUN_USERNAME: process.env.ACFUN_USERNAME ?? '',
-  ACFUN_PASSWORD: process.env.ACFUN_PASSWORD ?? ''
+  ACFUN_PASSWORD: process.env.ACFUN_PASSWORD ?? '',
+  MUSIC163_USERNAME: process.env.MUSIC163_USERNAME ?? '',
+  MUSIC163_PASSWORD: process.env.MUSIC163_PASSWORD ?? ''
 }
---
+++ src/tasks/music163.ts
@@ -0,0 +1,28 @@
+import puppeteer from 'puppeteer'
+import CONFIG from '../config'
+
+async function bootstrap() {
+  const browser = await puppeteer.launch({
+    headless: false
+  })
+  const [page] = await browser.pages()
+  await page.setViewport({ width: 1200, height: 900 })
+  await page.goto('https://music.163.com/')
+
+  // await page.click('#index-enter-default')
+  const [_, frame] = await page.frames()
+  const loginBtn = await frame.waitForSelector('#index-enter-default')
+  await loginBtn.click()
+  await page.click('#j-official-terms')
+  await page.click('.zcnt div.u-main > div:nth-child(2) > a')
+  await page.type('#p', CONFIG.MUSIC163_USERNAME)
+  await page.type('#pw', CONFIG.MUSIC163_PASSWORD)
+  await page.click('.zcnt div.n-log2.n-log2-2 > div.f-mgt20 > a')
+  const checkInBtn = await frame.waitForSelector(
+    '#discover-module > div.g-sd1 > div.n-myinfo.s-bg.s-bg-5 > div > div > div > a'
+  )
+  await checkInBtn.click()
+  await browser.close()
+}
+
+bootstrap()

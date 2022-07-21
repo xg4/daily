b04373927734d175cae6036ca6d61fc1ce977c25
xg4 <xingor4@gmail.com>
2022-07-21 14:03:29 +0800

    chore: add prisma schema

---
+++ prisma/migrations/20220721060140_init/migration.sql
@@ -0,0 +1,26 @@
+-- CreateTable
+CREATE TABLE "users" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "created_at" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" DATETIME NOT NULL,
+    "email" TEXT,
+    "username" TEXT NOT NULL
+);
+
+-- CreateTable
+CREATE TABLE "accounts" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "created_at" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" DATETIME NOT NULL,
+    "name" TEXT NOT NULL,
+    "cookie" TEXT,
+    "banned" BOOLEAN NOT NULL DEFAULT false,
+    "creatorId" INTEGER,
+    CONSTRAINT "accounts_creatorId_fkey" FOREIGN KEY ("creatorId") REFERENCES "users" ("id") ON DELETE SET NULL ON UPDATE CASCADE
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_email_key" ON "users"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_username_key" ON "users"("username");
